0aa8cdf1a8997e7647479b174a3b6ca9f91830fe
paulozaka <paulozakaluk@gmail.com>
2025-11-13 19:22:39 +0100

    Atualização: <base href=> - site no InfinityFree

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIMETRIA</title>
+    <base href="https://simetria-3d.xo.je/">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
